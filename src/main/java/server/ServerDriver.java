417a224d78ab5bd88ec28a649484c24f2f0fbe00
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-03-29 17:07:17 +1100

    基本连接监听

--- src/main/java/server/ServerDriver.java
+++ src/main/java/server/ServerDriver.java
@@ -1,34 +1,50 @@
 package server;
+
+import java.net.*;
+import java.io.*;
 public class ServerDriver {
-    public static final int DefaultPORT = 8888;
+    public static final int DefaultPORT = 8500;
 
     public static void main(String[] args) {
         int port = DefaultPORT;
         String dictFile = null;
-        String DEFAULTFILE = "dictionary.txt";
+        String DEFAULT_FILE = "dictionary.txt";
+        int connectedClients = 0;
 
         if (args.length < 2) {
             if (args.length == 1) {
-                dictFile = DEFAULTFILE;
-                ServerLogger.logGeneralErr(STR."Dictionary file not specified, using default file: ./\{DEFAULTFILE}");
+                dictFile = DEFAULT_FILE;
+                ServerLogger.logGeneralErr(STR."Dictionary file not specified, using default file: ./\{DEFAULT_FILE}");
             }else {
                 ServerLogger.logGeneralErr("Expected arguments: “port” “Dict file PATH”");
                 System.exit(1);
             }
         }
-
         try {
             port = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
             ServerLogger.logInvalidArgumentErr(args[0]);
             System.exit(1);
         }
+
+        dictFile = args[1];
+
         Dict dict = new Dict(dictFile);
-//        Dict = new Dict("dictionary.txt");
-        System.out.println(dict.search("banana"));
-        System.out.println(dict.add("appleLLL", "a fruit"));
-        System.out.println(dict.update("appleLLL", "a kind of fruit"));
-        dict.printDictionaryInfo();
-        dict.close();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+//            serverSocket.setSoTimeout(90000);
+            dict.add("server", serverSocket.toString());
+            while (connectedClients < 10) {
+                Socket clientSocket = serverSocket.accept();
+                dict.add(String.valueOf(connectedClients++), clientSocket.toString());
+            }
+            dict.close();
+        }catch (IOException ioe) {
+            ServerLogger.logGeneralErr(STR."Could not listen on port: \{port}");
+            System.exit(1);
+        }catch (IllegalArgumentException iae) {
+            ServerLogger.logGeneralErr(STR."Port number out of range: \{port}");
+            System.exit(1);
+        }
+//        dict.close();
     }
 }
