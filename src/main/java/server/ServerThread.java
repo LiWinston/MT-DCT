b5ea5feefe6eb87a25adb2ab58d34e9f1b3863ef
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-03-30 23:49:39 +1100

    Create ServerThread.java

---
+++ src/main/java/server/ServerThread.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+    public ServerThread(Socket clientSocket, Dict dict) {
+        
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
