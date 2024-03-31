53c53682cf122742e4883165e0d5579a8c09002a
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-03-31 21:38:38 +1100

    Update Response.java

--- src/main/java/prtc/Response.java
+++ src/main/java/prtc/Response.java
@@ -4,7 +4,30 @@ public class Response {
     public Response(boolean b, String msg) {
         status = b ? Status.SUCCESS : Status.FAIL;
         message = msg;
+        meanings = null;
     }
+    public Response(boolean b, String msg, String meanings) {
+        status = b ? Status.SUCCESS : Status.FAIL;
+        message = msg;
+        this.meanings = meanings;
+    }
+
+    public enum Status {
+        SUCCESS,
+        FAIL
+    }
+    final Status status;
+    final String message;
+
+    final String meanings;
+    //A response handler, not response class
+    public static String failResponse(String message) {
+        return "{\"status\":\"fail\",\"message\":\"" + message + "\"}";
+    }
+    public static String simpleSuccessResponse(String message) {
+        return "{\"status\":\"success\",\"message\":\"" + message + "\"}";
+    }
+
 
     public Status getStatus() {
         return status;
@@ -14,47 +37,7 @@ public class Response {
         return message;
     }
 
-    public enum Status {
-        SUCCESS,
-        FAIL
-    }
-    final Status status;
-    final String message;
-    //A response handler, not response class
-    public static String failResponse(String message) {
-        return "{\"status\":\"fail\",\"message\":\"" + message + "\"}";
-    }
-    public static String simpleSuccessResponse(String message) {
-        return "{\"status\":\"success\",\"message\":\"" + message + "\"}";
-    }
-
-    public static String searchResponse(String word, String[] meanings) {
-        StringBuilder response = new StringBuilder("{\"status\":\"success\",\"word\":\"" + word + "\",\"meanings\":[");
-        for (String meaning : meanings) {
-            response.append("\"").append(meaning).append("\",");
-        }
-        response.deleteCharAt(response.length() - 1);
-        response.append("]}");
-        return response.toString();
-    }
-    public static String addResponse(String word, String[] meanings) {
-        StringBuilder response = new StringBuilder("{\"status\":\"success\",\"word\":\"" + word + "\",\"meanings\":[");
-        for (String meaning : meanings) {
-            response.append("\"").append(meaning).append("\",");
-        }
-        response.deleteCharAt(response.length() - 1);
-        response.append("]}");
-        return response.toString();
-    }
-
-    public static void parseResponse(String response) {
-        switch (response.split(":")[1]){
-            case "success", "fail":
-                //TODO: log to caller
-
-                break;
-            default:
-                System.out.println("Invalid response");
-        }
+    public String getMeanings() {
+        return meanings;
     }
 }
