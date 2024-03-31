05404347e7923750de3799d2c911d080bf77f1eb
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-03-31 21:39:00 +1100

    Virtual task exc

--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -3,6 +3,8 @@ package server;
 import java.io.*;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.concurrent.Executors;
+
 import prtc.*;
 
 public class ServerThread implements Runnable {
@@ -23,43 +25,53 @@ public class ServerThread implements Runnable {
         try (DataInputStream in = new DataInputStream(socket.getInputStream());
              BufferedReader b_iStream = new BufferedReader(new InputStreamReader(in));
              DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-            while (true) {
+
+            try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                while (true) {
 //                System.out.println("Waiting for request...");
-                String req = b_iStream.readLine();
-                System.out.println("Request received: " + req);
-                // Decode the request
-                Request.Action action = localReqHdl.getAction(req);
+                    String req = b_iStream.readLine();
+                    System.out.println("Request received: " + req);
+                    // Decode the request
+                    Request.Action action = localReqHdl.getAction(req);
 
-                // Process the request
-                if (action != null) {
-                    switch (action) {
-                        case ADD:
-                            String word = localReqHdl.getWord(req);
-                            String meanings = localReqHdl.getMeanings(req);
-                            out.writeUTF(Response.Status.SUCCESS == dict.add(word, meanings).getStatus()?
-                                    Response.simpleSuccessResponse(STR."Word\{word} added successfully") :
-                                    Response.failResponse(STR."Word\{word} already exists"));
-                            break;
-                        case DELETE:
-                            word = localReqHdl.getWord(req);
-                            dict.delete(word);
-                            out.writeBytes("Word deleted successfully\n");
-                            break;
-                        case UPDATE:
-                            word = localReqHdl.getWord(req);
-                            meanings = localReqHdl.getMeanings(req);
-                            dict.update(word, meanings);
-                            out.writeBytes("Word updated successfully\n");
-                            break;
-                        case SEARCH:
-                            word = localReqHdl.getWord(req);
-                            System.out.println("Searching for word" + word);
+                    // Process the request
+                    if (action != null) {
+                        String word = localReqHdl.getWord(req);
+                        switch (action) {
+                            case ADD:
+                                String finalWord = word;
+                                executor.submit(() -> {
+                                    String meanings = localReqHdl.getMeanings(req);
+                                    try {
+                                        out.writeChars (Response.Status.SUCCESS == dict.add(finalWord, meanings).getStatus()?
+                                                Response.simpleSuccessResponse(STR."Word\{finalWord} added successfully") :
+                                                Response.failResponse(STR."Word\{finalWord} already exists"));
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                });
+                                break;
+                            case DELETE:
+                                word = localReqHdl.getWord(req);
+                                dict.delete(word);
+                                out.writeBytes("Word deleted successfully\n");
+                                break;
+                            case UPDATE:
+                                word = localReqHdl.getWord(req);
+                                String meanings = localReqHdl.getMeanings(req);
+                                dict.update(word, meanings);
+                                out.writeBytes("Word updated successfully\n");
+                                break;
+                            case SEARCH:
+                                word = localReqHdl.getWord(req);
+                                System.out.println("Searching for word" + word);
 
-                            String result = dict.search(word);
-                            out.writeUTF(result + "\n");
-                            break;
-                        default:
-                            out.writeBytes("Invalid request\n");
+                                String result = dict.search(word);
+                                out.writeBytes(result + "\n");
+                                break;
+                            default:
+                                out.writeBytes("Invalid request\n");
+                        }
                     }
                 }
             }
