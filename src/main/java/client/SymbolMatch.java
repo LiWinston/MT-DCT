42496f67519e7b6f02a376fb46d6aaca2c97fda0
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-04-05 10:28:35 +1100

    Update SymbolMatch.java

--- src/main/java/client/SymbolMatch.java
+++ src/main/java/client/SymbolMatch.java
@@ -1,8 +1,10 @@
 package client;
 
 import java.util.Stack;
+import java.util.function.Predicate;
 
 public class SymbolMatch {
+    static Predicate<Character> keepCharacter = c -> Character.isLetter(c) || c == ' ' || "+=,.!-_$#@^&*?\"".indexOf(c) != -1;
     public static void main(String[] args) {
         System.out.println(checkParenthesesMatchandDeleteIllegal("dfsds[][[]]]")); // 示例调用
     }
@@ -14,16 +16,16 @@ public class SymbolMatch {
         // 遍历字符串中的每个字符
         for (char c : target.toCharArray()) {
             // 如果是字母文字，直接添加到结果中
-            if (Character.isLetter(c)) {
+            if (keepCharacter.test(c)) {
                 result.append(c);
             }
             // 如果是左括号，入栈
-            else if (c == '(' || c == '[' || c == '{') {
+            else if (c == '(' || c == '[' || c == '{'|| c == '<') {
                 stack.push(c);
                 result.append(c);
             }
             // 如果是右括号
-            else if (c == ')' || c == ']' || c == '}') {
+            else if (c == ')' || c == ']' || c == '}' || c == '>') {
                 // 如果栈为空，或者栈顶元素与当前右括号不匹配，则将当前右括号视为非法，并继续下一个字符
                 if (stack.isEmpty() || !isMatching(stack.peek(), c)) {
                     continue;
@@ -48,6 +50,7 @@ public class SymbolMatch {
     private static boolean isMatching(char left, char right) {
         return (left == '(' && right == ')') ||
                 (left == '[' && right == ']') ||
-                (left == '{' && right == '}');
+                (left == '{' && right == '}') ||
+                (left == '<' && right == '>');
     }
 }
