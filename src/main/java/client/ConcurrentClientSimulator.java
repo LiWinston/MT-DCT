894d2570e144a82c21644ff0be49c5cd9edc95b7
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-04-05 16:32:48 +1100

    Update ConcurrentClientSimulator.java

--- src/main/java/client/ConcurrentClientSimulator.java
+++ src/main/java/client/ConcurrentClientSimulator.java
@@ -4,8 +4,8 @@ public class ConcurrentClientSimulator {
     public static void main(String[] args) {
         //自动生成Client线程并随机生成请求
         for (int i = 0; i < 4; i++) {
-            Client client = new Client();
-            Client.main(new String[]{"localhost", "8500"});
+//            Client client = new Client();
+            Client.main(new String[]{"localhost", "8600"});
         }
     }
 }
